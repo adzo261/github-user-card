880488e290a35a2733215fc757725d1095948eb8
Aditya Zope <adityazope261@gmail.com>
2018-12-23 11:22:26 +0530

    Updates

---
+++ precache-manifest.0bb7f4d6a0865d1347423469f777a861.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c4d07acf727201b06fe",
+    "url": "/github-user-card/static/js/runtime~main.9c4d07ac.js"
+  },
+  {
+    "revision": "8bf945df214f99038bdd",
+    "url": "/github-user-card/static/js/main.8bf945df.chunk.js"
+  },
+  {
+    "revision": "0a395804ba555842ec76",
+    "url": "/github-user-card/static/js/1.0a395804.chunk.js"
+  },
+  {
+    "revision": "8bf945df214f99038bdd",
+    "url": "/github-user-card/static/css/main.c3649051.chunk.css"
+  },
+  {
+    "revision": "cde63769127901c56e8eb76e4456029a",
+    "url": "/github-user-card/index.html"
+  }
+];
