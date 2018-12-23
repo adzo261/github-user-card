083799f67cfdbd3537411042716451a667054246
Aditya Zope <adityazope261@gmail.com>
2018-12-23 11:16:28 +0530

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import "./App.css";
+import SearchBar from "./SearchBar";
+import Card from "./Card";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchQuery: "",
+      didSubmit: false
+    };
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onSubmit() {
+    this.setState({
+      didSubmit: true
+    });
+  }
+  onChange(event) {
+    this.setState({
+      searchQuery: event.target.value,
+      didSubmit: false
+    });
+  }
+
+  render() {
+    const { searchQuery } = this.state;
+    return (
+      <div className="App">
+        <SearchBar
+          onChange={this.onChange}
+          onClick={this.onSubmit}
+          value={searchQuery}
+        />
+        {this.state.didSubmit && <Card username={searchQuery} />}
+      </div>
+    );
+  }
+}
+
+export default App;
